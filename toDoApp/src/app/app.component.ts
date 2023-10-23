1631c90207a088055910cc747c031592808f1b1b
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2023-10-23 23:09:15 +0300

    Angular #2

--- toDoApp/src/app/app.component.ts
+++ toDoApp/src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'toDoApp';
+  title = 'toDo App';
+
+  getTitle(){
+    return this.title
+  }
+
+  toDoItem = {
+    description : 'kahvaltı',
+    action : true
+  }
 }
