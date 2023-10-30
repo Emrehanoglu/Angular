f2613686e4d1ff5dff5541010ad5d63fa5809454
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2023-10-30 23:40:08 +0300

    Movie App #23

--- MovieApp/src/app/movie-create/movie-create.component.ts
+++ MovieApp/src/app/movie-create/movie-create.component.ts
@@ -3,6 +3,7 @@ import { CategoryService } from '../services/category.service';
 import { Category } from '../models/category';
 import { MovieService } from '../services/movie.service';
 import { Router } from '@angular/router';
+import { AlertifyService } from '../services/alertify.service';
 
 @Component({
   selector: 'app-movie-create',
@@ -14,7 +15,7 @@ export class MovieCreateComponent implements OnInit {
 
   categories : Category[]
 
-  constructor(private categoryService:CategoryService, private movieService:MovieService, private router:Router) { }
+  constructor(private categoryService:CategoryService, private movieService:MovieService, private router:Router, private alertify:AlertifyService) { }
 
   ngOnInit(): void { /* ekran ilk açılırken yanı daha butona basmadım post işlemi yapmadım, bir nevi HttpGet gibi düşün */
     this.categoryService.getCategories().subscribe(data => {
@@ -23,7 +24,21 @@ export class MovieCreateComponent implements OnInit {
   }
 
   createMovie(title:any,description:any,imageUrl:any,categoryId:any){
-
+    if(title.value==="" || description.value==="" || imageUrl.value==="" || categoryId.value==="-1"){
+      this.alertify.error("Bir veya birden fazla alan doldurulmamış gözünüyor")
+      return
+    }
+    if(title.value.length < 10){
+      this.alertify.error("Title en az 10 karakterden oluşmalıdır")
+      return
+    }
+    const extensions = ["jpeg","png","jpg"]
+    const extension = imageUrl.value.split('.').pop() /* gelen imageUrl değerini noktadan ayır ve son elemanı ele al(pop: son elemanı secmemızı sağlar) */
+    if(extensions.indexOf(extension) === -1){
+      this.alertify.error('İmageUrl girişi hatalı, lütfen tekrar deneyiniz')
+      return 
+    }
+    
     const movie = {
       id:0,
       title: title.value,
