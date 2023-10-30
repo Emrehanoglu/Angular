ca693ebfa39d2db7301d0b5d03383f2dc92d3daa
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2023-10-30 22:42:19 +0300

    Movie App #21

--- MovieApp/src/app/app-routing.module.ts
+++ MovieApp/src/app/app-routing.module.ts
@@ -3,12 +3,15 @@ import { CommonModule } from '@angular/common';
 import { RouterModule,Routes } from '@angular/router';
 import { MoviesComponent } from './movies/movies.component';
 import { MovieDetailsComponent } from './movie-details/movie-details.component';
+import { MovieCreateComponent } from './movie-create/movie-create.component';
 
 const routes:Routes = [ /* burada url path lerimi tanımlıyorum, hangi path gelirse hangi componente gitsin ve oradaki html kodları ile önyüzde gosterilsin */
   {path:'movies', component:MoviesComponent}, /* 4200(projenın ayaga kalktığı standart port) portu çağırılırsa movies.component.ts 'e gitsin */
   {path: '', redirectTo:'movies',pathMatch:'full'}, /* url üzerinden bir şey yazılmadan gidilmeye calısırsa yıne movies 'e yonlendır */
   {path: 'movies/category/:categoryId', component:MoviesComponent},  /* göndermiş olduğum id bilgisine göre movies.component.ts içerisinde filtreleme yapacağım 
                                                                         id bilgisinide category.component.html içerisinde routerLink ile vereceğim*/
+  
+  {path: 'movies/create', component:MovieCreateComponent},
   {path: 'movies/:movieId', component:MovieDetailsComponent}
 ]
 
---
+++ MovieApp/src/app/movie-create/movie-create.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { CategoryService } from '../services/category.service';
+import { Category } from '../models/category';
+
+@Component({
+  selector: 'app-movie-create',
+  templateUrl: './movie-create.component.html',
+  styleUrls: ['./movie-create.component.css'],
+  providers:[CategoryService]
+})
+export class MovieCreateComponent implements OnInit {
+
+  categories : Category[]
+
+  constructor(private categoryService:CategoryService) { }
+
+  ngOnInit(): void {
+    this.categoryService.getCategories().subscribe(data => {
+      this.categories = data
+    })
+  }
+
+  createMovie(title:any,description:any,imageUrl:any,categoryId:any){
+    console.log(title.value)
+    console.log(description.value)
+    console.log(imageUrl.value)
+    console.log(categoryId.value)
+  }
+
+}
