74c55f36e144d308d781c9644312c94d119afa17
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2023-11-01 23:04:52 +0300

    Movie App #29/Firebase #2

--- MovieApp/src/app/services/movie.service.ts
+++ MovieApp/src/app/services/movie.service.ts
@@ -2,22 +2,36 @@ import { HttpClient, HttpHeaders } from "@angular/common/http"
 import {Injectable} from "@angular/core"
 import { Observable } from "rxjs"
 import { Movie } from "../models/movie"
+import { catchError, map, tap } from "rxjs/operators";
 
 @Injectable() /* bu service 'i kullanacağım componentin ctor 'unda dahil edip kullanabilmem için */
 
 export class MovieService{
 
     url = 'http://localhost:3000/movies'
-    url_firebase = 'https://angular-movie-app-c3aaf-default-rtdb.firebaseio.com'
+    url_firebase = 'https://angular-movie-app-c3aaf-default-rtdb.firebaseio.com/'
 
     constructor(private http:HttpClient){}
 
     getMovies(categorId:number) : Observable<Movie[]>{
-        let newUrl = this.url
+        let newUrl = this.url_firebase + 'movies.json'
         if(categorId){
             newUrl += '?categoryId=' + categorId
         }        
-        return this.http.get<Movie[]>(newUrl)
+        return this.http.get<Movie[]>(newUrl).pipe(
+            map(response => { /* map ile bana gelen objeyi manipüle edeceğim */
+                const movies: Movie[] = []
+
+                for(const key in response){
+                    movies.push({...response[key], id:key}) 
+                    /* 
+                        ...response[key] şu anlama geliyor, bana gelen respone içerisindeki elemanları yine aynı şekilde al, fakat 
+                        ikinci parametre id 'ye farklı bir değer ataması yapacağımı söylüyorum.  
+                    */
+                }
+                return movies
+            })
+        )
     }
 
     getMovieById(movieId:number):Observable<Movie>{
