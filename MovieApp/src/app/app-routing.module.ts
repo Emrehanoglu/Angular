54b739195a6c4db0ce196a1b80551870717c549d
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2023-11-12 01:07:32 +0300

    Movie App #41/Guard

--- MovieApp/src/app/app-routing.module.ts
+++ MovieApp/src/app/app-routing.module.ts
@@ -6,16 +6,17 @@ import { MovieDetailsComponent } from './movie-details/movie-details.component';
 import { MovieCreateComponent } from './movie-create/movie-create.component';
 import { CategoryCreateComponent } from './category-create/category-create.component';
 import { AuthComponent } from './auth/auth.component';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes:Routes = [ /* burada url path lerimi tanımlıyorum, hangi path gelirse hangi componente gitsin ve oradaki html kodları ile önyüzde gosterilsin */
-  {path:'movies', component:MoviesComponent}, /* 4200(projenın ayaga kalktığı standart port) portu çağırılırsa movies.component.ts 'e gitsin */
+  {path:'movies', component:MoviesComponent, canActivate:[AuthGuard]}, /* 4200(projenın ayaga kalktığı standart port) portu çağırılırsa movies.component.ts 'e gitsin */
   {path: '', redirectTo:'movies',pathMatch:'full'}, /* url üzerinden bir şey yazılmadan gidilmeye calısırsa yıne movies 'e yonlendır */
-  {path: 'movies/category/:categoryId', component:MoviesComponent},  /* göndermiş olduğum id bilgisine göre movies.component.ts içerisinde filtreleme yapacağım 
+  {path: 'movies/category/:categoryId', component:MoviesComponent, canActivate:[AuthGuard]},  /* göndermiş olduğum id bilgisine göre movies.component.ts içerisinde filtreleme yapacağım 
                                                                         id bilgisinide category.component.html içerisinde routerLink ile vereceğim*/
   
-  {path: 'movies/create', component:MovieCreateComponent},
-  {path: 'categories/create', component:CategoryCreateComponent},
-  {path: 'movies/:movieId', component:MovieDetailsComponent},
+  {path: 'movies/create', component:MovieCreateComponent, canActivate:[AuthGuard]},
+  {path: 'categories/create', component:CategoryCreateComponent, canActivate:[AuthGuard]},
+  {path: 'movies/:movieId', component:MovieDetailsComponent, canActivate:[AuthGuard]},
   {path: 'auth', component:AuthComponent}
 ]
 
---
+++ MovieApp/src/app/guards/auth.guard.ts
@@ -0,0 +1,25 @@
+import { Injectable } from "@angular/core";
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
+import { Observable } from "rxjs";
+import { AuthService } from "../services/auth.service";
+import { map, tap } from "rxjs/operators";
+
+@Injectable({providedIn:'root'}) /* root ile global bir guard olacağını belirttim */
+export class AuthGuard implements CanActivate{
+    constructor(private authService:AuthService, private router:Router){}
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+        return this.authService.user.pipe(
+            map(user =>{
+                return !!user 
+                /* user bilgisi var ise değili false olur, onun da değili true olur */    
+                /* user bilgisi yok ise değili true olur, onun da değili false olur */ 
+                /* bunu yapmamızın nedeni normalde user bilgisi boolean bir veri tipi olmadığı için bu şekilde kullanıma gidildi. */   
+            }),
+            tap(isAuth =>{ /* isAuth değeri yukarıdaki user dan dönecek olan true ya da false ile setlenecek */
+                if(!isAuth){ /* false ise yani user bilgisi yok ise */
+                    this.router.navigate(["/auth"])
+                }
+            })
+        )
+    }
+}
