7dce897fafe39d6ec9c6ae52a9ef35084ab1b219
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2023-11-12 00:05:57 +0300

    Movie App #40/AutoLogin

--- MovieApp/src/app/services/auth.service.ts
+++ MovieApp/src/app/services/auth.service.ts
@@ -23,12 +23,7 @@ export class AuthService {
       returnSecureToken:true /* true gönderilmeli. Firebase sitesinde bu bilgi var zaten, böyle gönderilmeli diyor. */
     }).pipe(
       tap(response => {
-        const expirationDate = new Date(new Date().getTime() + Number(response.expiresIn)*1000)
-        const user = new User(response.email,response.localId,response.idToken,expirationDate)
-        this.user.next(user) 
-        /* projedeki herhangi bir fonksiyonda subscribe olunan data içerisinde, gözlemciler aracılığı ile user
-        bilgilerine ulaşabiliyor olacağız. subscibe olunan kısımı gözlemci olarak anlayabilirsin. Sonucta bir akış üzerindeki
-        datayı alıp inceliyoruz orada, gözlemliyoruz. */
+        this.handleAuthentication(response.email,response.localId,response.idToken,+response.expiresIn) /* + ile string to number yaptım */
       })
     )
   }
@@ -39,17 +34,39 @@ export class AuthService {
       returnSecureToken:true /* true gönderilmeli. Firebase sitesinde bu bilgi var zaten, böyle gönderilmeli diyor. */
     }).pipe(
       tap(response => {
-        const expirationDate = new Date(new Date().getTime() + Number(response.expiresIn)*1000)
-        const user = new User(response.email,response.localId,response.idToken,expirationDate)
+        this.handleAuthentication(response.email,response.localId,response.idToken,+response.expiresIn) /* + ile string to number yaptım */
+      })
+    )
+  }
+  handleAuthentication(email:string,userId:string,token:string,expiresIn:number){
+    const expirationDate = new Date(new Date().getTime() + expiresIn*1000)
+        const user = new User(email,userId,token,expirationDate)
         this.user.next(user) 
         /* projedeki herhangi bir fonksiyonda subscribe olunan data içerisinde, gözlemciler aracılığı ile user
         bilgilerine ulaşabiliyor olacağız. subscibe olunan kısımı gözlemci olarak anlayabilirsin. Sonucta bir akış üzerindeki
         datayı alıp inceliyoruz orada, gözlemliyoruz. */
-      })
+
+        localStorage.setItem("user",JSON.stringify(user)) /* user bilgisini direk olarak saklayamam, string şeklinde saklamam gerekiyor */
+  }
+  autoLogin(){
+    const user = JSON.parse(localStorage.getItem("user")) /* user key i altındaki bilgileri aldım */
+    if(!user){
+      return
+    }
+    const loadedUser = new User(
+      user.email,
+      user.id,
+      user._token,
+      new Date(user._tokenExpirationDate)
     )
+    if(loadedUser.token){
+      this.user.next(loadedUser)
+    }
   }
   logout(){
     this.user.next(null)
+    localStorage.removeItem("user") /* LS içerisinde user key'i altında tutulan veriler silinsin. */
     this.route.navigate(['/auth'])
   }
+  
 }
