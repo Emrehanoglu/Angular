b9a1aa5a62b1c8b3ec673c29237f087fd5f4d586
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2023-11-15 20:17:30 +0300

    Shop App #12

--- ShopApp/src/app/model/model.module.ts
+++ ShopApp/src/app/model/model.module.ts
@@ -4,16 +4,20 @@ import { RestService } from "./rest.service";
 import { ProductRepository } from "./product.repository";
 import { CategoryRepository } from "./category.repository";
 import { Cart, CartItem } from "./cart.model";
+import { Order } from "./order.model";
+import { OrderRepository } from "./order.repository";
 
 @NgModule({
     imports: [
         HttpClientModule /* http ile api servisine req gonderebilmek için bu module 'ü ekledim */
     ],
-    providers:[ /* serv,sler burada olacak */
+    providers:[ /* servisler burada olacak */
         RestService,
         ProductRepository,
         CategoryRepository,
-        Cart
+        Cart,
+        Order,
+        OrderRepository
     ]
 })
 
---
+++ ShopApp/src/app/model/order.model.ts
@@ -0,0 +1,25 @@
+import { Injectable } from "@angular/core"
+import { Cart } from "./cart.model"
+
+@Injectable()
+export class Order{
+    public id: any
+    public name: any
+    public address: any
+    public city: any
+    public phone: any
+    public email: any
+
+    public isSent?: boolean=false
+    constructor(public cart:Cart){}
+    clearOrder(){ /* sipariş verildiği anda artık sipariş bilgileri sıfırlanabilir */
+        this.id = null
+        this.name = null
+        this.address = null
+        this.city = null
+        this.phone = null
+        this.email = null
+        this.isSent = false
+        this.cart.clear() /* sipariş verildiği anda sepet sıfırlanabilir */
+    }
+}
